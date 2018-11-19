441b244990b48c301341f105efef5fa0010f973b
Tymeckia Kendall <tykend@gmail.com>
2018-11-19 20:36:48 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
  def parrot(phrase = "Squawk!")
-   "Prettybird!"= "Squawk"
+   "Prettybird!"="Squawk"
    puts "#{phrase}"
   
 end
